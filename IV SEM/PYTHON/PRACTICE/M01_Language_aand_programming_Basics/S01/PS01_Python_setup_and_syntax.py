71498a303db401bced086875c54d142ebf09fc3b
bharath kumar <bharathgatla14@gmail.com>
2026-02-06 14:40:04 +0530

    message

---
+++ IV SEM/PYTHON/PRACTICE/M01_Language_aand_programming_Basics/S01/PS01_Python_setup_and_syntax.py
@@ -0,0 +1,4 @@
+user_name = "G.Bharath kumar"
+print(user_name) 
+and14 = 2006
+print(and14)
---
+++ IV SEM/PYTHON/PRACTICE/M01_Language_aand_programming_Basics/S01/PS02_Variables_and_data_types.py
@@ -0,0 +1,30 @@
+'''
+Data type:
+1.Fundamental DT:
+  1.int
+  2.Float
+  3.complex
+  4.Boolean
+  5.String
+
+2.Collection Dt:
+  1.List
+  2.Tuple
+  3.set
+  4.Dictionary
+
+'''
+x = 10
+y = 12e4
+print(x,type(x))
+print(y,type(y))
+
+c1 = 4+5j
+c2 = complex (2,-2)
+print(c1.real)
+print(c1.imag)
+print(c1 + c2)
+print(c1-c2)
+print(c1*c2)
+print(c1/c2)
+
