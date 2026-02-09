158eb7a3d96757be4d14b833e9b58f3744cebc7a
bharath kumar <bharathgatla14@gmail.com>
2026-02-09 14:55:09 +0530

    mes1

---
+++ IV SEM/PYTHON/PRACTICE/M01_Language_aand_programming_Basics/S01/PS04_conditional_statements.py
@@ -0,0 +1,9 @@
+n = int(input())
+if n>0:
+    print("+ve")
+elif n == 0:
+    print("zero")
+else: 
+    print("-ve")
+    
+
---
+++ IV SEM/PYTHON/PRACTICE/M02_Logiuc Building_&_Patterns/PS02_Factors&Primes.py
@@ -0,0 +1,12 @@
+
+n = int(input("Enter a number: "))  # shows prompt in terminal
+count = 0
+
+print("Factors of", n, "are:")
+for i in range(1, n + 1):
+    if n % i == 0:
+        count += 1
+        print(i, end=" ")
+
+print("\nTotal number of factors:", count)
+
